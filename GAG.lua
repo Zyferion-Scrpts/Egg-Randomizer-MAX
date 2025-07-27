7204ce49b5c378f6a76f4d1a8fb3be7ca7b8f698
Zyferion-Scrpts <uzilia769@gmail.com>
2025-07-27 12:38:02 +0800

    Update GAG.lua

--- GAG.lua
+++ GAG.lua
@@ -1,3 +1,4 @@
 loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/glkOoNFU9FalXS6w/raw", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Renasaix/Pet-Randomizer/main/GAG.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Renasaix/Egg-RandomizerV2/main/GAG.lua"))()
+
 
